cd4534d2e3ba96dabb7d128f6337959d44bff628
siddheshvk <kuvalekar.s@husky.neu.edu>
2019-03-14 19:52:15 -0400

    image and install

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+yum install java-1.8.0-openjdk.x86_64 -y
 yum install -y wget
 cd /tmp
-wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
-rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+sudo wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+sudo rpm -ivh mysql57-community-release-el7-9.noarch.rpm
 yum install -y mysql-server
 yum install -y mysql
 yum install -y maven
@@ -11,7 +12,7 @@ systemctl start mysqld
 yum -y update
 yum install -y ruby
 wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
-chmod +x ./install
+sudo chmod +x ./install
 ./install auto
 service codedeploy-agent status
 service codedeploy-agent start
@@ -19,98 +20,30 @@ service codedeploy-agent status
 
 
 sleep 10
-
-sudo yum install java-1.7.0-openjdk-devel -y
-sudo groupadd tomcat
-sudo useradd -M -s /bin/nologin -g tomcat -d /opt/tomcat tomcat
-sudo yum install wget
-cd ~
-wget https://www.apache.org/dist/tomcat/tomcat-9/v9.0.16/bin/apache-tomcat-9.0.16.tar.gz
-sudo mkdir /opt/tomcat
-sudo tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1
-cd /opt/tomcat
-sudo chgrp -R tomcat /opt/tomcat
-sudo chmod -R g+r conf
-sudo chmod g+x conf
-sudo chown -R tomcat webapps/ work/ temp/ logs/
+mkdir /opt/tomcat
+wget -q --no-cookies -S "http://www.trieuvan.com/apache/tomcat/tomcat-9/v9.0.16/bin/apache-tomcat-9.0.16.tar.gz"
+tar -xf apache-tomcat-9.0.16.tar.gz
+sudo mv apache-tomcat-9.0.16/ /opt/tomcat/
+sudo chmod 777 ~/.bashrc
+echo "export CATALINA_HOME='/opt/tomcat/'" >> ~/.bashrc
+sudo useradd -r tomcat --shell /bin/false
+sudo chown -R tomcat:tomcat /opt/tomcat/
 cat > /etc/systemd/system/tomcat.service << EOF
-# Systemd unit file for tomcat
 [Unit]
-Description=Apache Tomcat Web Application Container
+Description= Tomcat 9
 After=syslog.target network.target
-
 [Service]
-Type=forking
-
-Environment=JAVA_HOME=/usr/lib/jvm/jre
-Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
-Environment=CATALINA_HOME=/opt/tomcat
-Environment=CATALINA_BASE=/opt/tomcat
-Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
-
-ExecStart=/opt/tomcat/bin/startup.sh
-ExecStop=/bin/kill -15 $MAINPID
-
 User=tomcat
 Group=tomcat
-UMask=0007
-RestartSec=10
-Restart=always
-
+Type=forking
+Environment=JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk
+Environment=CATALINA_PID=/opt/tomcat/apache-tomcat-9.0.16/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/tomcat/apache-tomcat-9.0.16
+Environment=CATALINA_BASE=/opt/tomcat/apache-tomcat-9.0.16
+Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd:/dev/./urandom'
+ExecStart=/opt/tomcat/apache-tomcat-9.0.16/bin/startup.sh
+ExecStop=/bin/kill -15 $MAINPID
+Restart=on-failure
 [Install]
-WantedBy=multi-user.target
+wantedBy=multi-user.target
 EOF
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
